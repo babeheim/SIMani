b85fa4a6783572cd4898e0db927e83fb3a451967
Bret Beheim <beheim@gmail.com>
2020-12-04 15:08:47 +0100

    first commit of new version, with working functions for migration and mortality

---
+++ R/model_functions.R
@@ -0,0 +1,76 @@
+
+increment_age <- function(people) {
+  if (!"age_today" %in% names(people)) stop("age_today does not exist")
+  if (any(is.na(people$age_today))) stop("age_today has missing values")
+  if (!"is_dead" %in% names(people)) stop("is_dead does not exist")
+  if (any(is.na(people$is_dead))) stop("is_dead has missing values")
+  if (any(!people$is_dead)) {
+    people$age_today[!people$is_dead] <- people$age_today[!people$is_dead] + 1
+  }
+  return(people)
+}
+
+add_immigrants <- function(people, day, n_immigrants = NA) {
+  if (is.na(n_immigrants)) n_immigrants <- rpois(1, 3)
+  if (n_immigrants > 0) {
+    immigrants <- vector("list", n_immigrants)
+    new_person <- vector("list", ncol(people))
+    names(new_person) <- names(people)
+    for (i in seq_along(new_person)) new_person[[i]] <- NA
+    new_person$is_dead <- FALSE
+    new_person$is_present <- TRUE
+    for (i in 1:n_immigrants) {
+      immigrants[[i]] <- new_person
+      immigrants[[i]]$name <- "A"
+      immigrants[[i]]$age_today <- rpois(1, 10)
+      immigrants[[i]]$date_of_birth <- day - immigrants[[i]]$age_today
+    }
+    immigrants %>% bind_rows() %>% as.data.frame() -> immigrants
+    people <- bind_rows(people, immigrants)
+  }
+  return(people)
+}
+
+
+select_emigrants <- function(people, day, emi_rows = NA) {
+  # choose emigrants probabilistically
+  active_rows <- which(!people$is_dead & people$is_present)
+  baseline <- 0.001
+  alpha <- log(baseline/(1 - baseline))
+  daily_pr_emigrate <- (1 - (1/(1 + exp(alpha))))
+  emigrated <- as.logical(rbinom(length(active_rows), 1, daily_pr_emigrate))
+  emigrated_rows <- active_rows[emigrated]
+  # add additional emigrations for testing
+  if (!all(is.na(emi_rows))) {
+    cannot_emigrate <- setdiff(emi_rows, active_rows)
+    if (length(cannot_emigrate) > 0) stop("only active people can emigrate")
+    emigrated_rows <- c(emigrated_rows, emi_rows)
+  }
+  if (length(emigrated_rows) > 0) {
+    people$is_present[emigrated_rows] <- FALSE
+  }
+  return(people)
+}
+
+select_fatalities <- function(people, day, kill_rows = NA) {
+  # choose deaths probabilistically
+  active_rows <- which(!people$is_dead)
+  if (length(active_rows) > 0) {
+    baseline <- 0.001
+    alpha <- log(baseline/(1 - baseline))
+    daily_pr_death <- (1 - (1/(1 + exp(alpha))))
+    died <- as.logical(rbinom(length(active_rows), 1, daily_pr_death))
+    died_rows <- active_rows[died]
+    # add additional deaths to test
+    if (!all(is.na(kill_rows))) {
+      cannot_die <- setdiff(kill_rows, active_rows)
+      if (length(cannot_die) > 0) stop("only active people can die")
+      died_rows <- c(died_rows, kill_rows)
+    }
+    if (length(died_rows) > 0) {
+      people$date_of_death[died_rows] <- day
+      people$is_dead[died_rows] <- TRUE
+    }
+  }
+  return(people)
+}
---
+++ tests/testthat/test_basics.R
@@ -0,0 +1,85 @@
+
+test_that("increment_age function works", {
+
+  test <- list(
+    list(name = "A", age_today = 1, is_dead = FALSE),
+    list(name = "B", age_today = 2, is_dead = FALSE),
+    list(name = "C", age_today = 1, is_dead = TRUE)
+  ) %>% bind_rows() %>% as.data.frame()
+
+  test <- increment_age(test)
+  expect_true(test$age_today[test$name == "A"] == 2)
+  expect_true(test$age_today[test$name == "B"] == 3)
+  expect_true(test$age_today[test$name == "C"] == 1)
+
+  test2 <- list(
+    list(name = "A", age_today = 1, is_dead = TRUE),
+    list(name = "B", age_today = 2, is_dead = TRUE),
+    list(name = "C", age_today = 1, is_dead = TRUE)
+  ) %>% bind_rows() %>% as.data.frame()
+
+  test2 <- increment_age(test2)
+  expect_true(test2$age_today[test2$name == "A"] == 1)
+  expect_true(test2$age_today[test2$name == "B"] == 2)
+  expect_true(test2$age_today[test2$name == "C"] == 1)
+
+})
+
+
+test_that("add_immigrants works", {
+
+  day <- 1
+  test <- list(
+    list(name = "A", age_today = 1, is_dead = FALSE),
+    list(name = "B", age_today = 2, is_dead = FALSE),
+    list(name = "C", age_today = 1, is_dead = TRUE)
+  ) %>% bind_rows() %>% as.data.frame()
+
+  test <- add_immigrants(test, day, 3)
+  expect_true(nrow(test) == 6)
+
+  day <- 1
+  test2 <- list(
+    list(name = "A", age_today = 1, is_dead = FALSE),
+    list(name = "B", age_today = 2, is_dead = FALSE),
+    list(name = "C", age_today = 1, is_dead = TRUE)
+  ) %>% bind_rows() %>% as.data.frame()
+
+  test2 <- add_immigrants(test2, day)
+  expect_true(nrow(test2) > 3)
+
+})
+
+
+test_that("select_emigrants works", {
+
+  day <- 1
+  test <- list(
+    list(name = "A", age_today = 1, date_of_death = NA, is_dead = FALSE, is_present = TRUE),
+    list(name = "B", age_today = 2, date_of_death = NA, is_dead = FALSE, is_present = TRUE),
+    list(name = "C", age_today = 1, date_of_death = 0, is_dead = TRUE, is_present = FALSE)
+  ) %>% bind_rows() %>% as.data.frame()
+
+  expect_error(select_emigrants(test, 1, 3))
+  test <- select_emigrants(test, 1, 1)
+  expect_false(test$is_present[1])
+  test <- select_emigrants(test, 1, 2)
+  expect_false(test$is_present[2])
+})
+
+
+test_that("select_fatalities works", {
+  day <- 1
+  test <- list(
+    list(name = "A", age_today = 1, date_of_death = NA, is_dead = FALSE, is_present = TRUE),
+    list(name = "B", age_today = 2, date_of_death = NA, is_dead = FALSE, is_present = TRUE),
+    list(name = "C", age_today = 1, date_of_death = 0, is_dead = TRUE, is_present = FALSE)
+  ) %>% bind_rows() %>% as.data.frame()
+
+  expect_error(select_fatalities(test, 1, 3))
+  test <- select_fatalities(test, 1, 1)
+  expect_true(test$is_dead[1])
+  test <- select_fatalities(test, 1, 2)
+  expect_true(test$is_dead[2])
+})
+
