a68ad581a35b69b723ecf8fe5f919f8c763cac78
Bret Beheim <beheim@gmail.com>
2020-12-07 16:58:21 +0100

    added a check to make sure ages are realistic

--- R/develop_functions.R
+++ R/develop_functions.R
@@ -8,9 +8,14 @@ increment_age <- function(people, tic_length = 1) {
   if (any(is.na(people$is_alive))) stop("is_alive has missing values")
   if (!"age" %in% names(people)) stop("age does not exist")
   living <- which(people$is_alive)
-  if (any(is.na(people$age[living]))) stop("living age values are missing")
   if (any(people$is_alive)) {
+    if (any(is.na(people$age[living]))) stop("living age values are missing")
+    if (any(people$age[living] < 0)) stop("living age values are negative")
     people$age[living] <- people$age[living] + (tic_length / 365)
+  }
+  active <- which(people$is_alive & people$is_present)
+  if (length(active) > 0) {
+    if (any(people$age[active] > 120)) warning("active age values are implausibly high")
   }
   return(people)
 }
